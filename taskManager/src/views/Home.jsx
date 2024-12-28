586226263c394d4dc80c9218a9add3d3dd2cce2b
AlexanderGC <alxander89@gmail.com>
2024-12-28 15:05:04 -0300

    Configure proyect

---
+++ taskManager/src/views/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <div>
+            <h1 className="text-5xl font-bold">
+                Hello world!
+            </h1>
+        </div>
+    )
+}
+
+export default Home
